c8db700dbef3f70171a09d99b71c5609b29714b8
= <=>
2022-08-09 14:06:14 +0100

    feat: add 01-rand_list.py and readme.md

---
+++ 06-lists/01-rand_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import random
+import math
+
+rand_list = []
+for i in range(1,6):
+    rand_list.append(random.randint(1,9))
+print(rand_list)
---
+++ 06-lists/README.md
@@ -0,0 +1,6 @@
+##Concepts
+- Lists
+- Bubble Sort
+- List comprehension
+##Tasks
+- Task 1:  Create a list of 5 random intergers from 1-9
